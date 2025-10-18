471c3b744627c5cde642cceb61ced26d319bbf90
Tushar Barua <tusharbarua269@gmail.com>
2025-10-18 18:58:35 +0600

    Update next.config.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -50,29 +50,65 @@
 
 
 
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
-const options = {};
+// const uri = process.env.MONGODB_URI!;
+// const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
+// let client;
+// let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error("Please add your Mongo URI to .env.local");
+// }
 
-if (process.env.NODE_ENV === "development") {
-  // Dev mode
-  if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
-  }
-  clientPromise = (global as any)._mongoClientPromise;
-} else {
-  // Prod mode
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   // Dev mode
+//   if (!(global as any)._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     (global as any)._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = (global as any)._mongoClientPromise;
+// } else {
+//   // Prod mode
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
 
-export default clientPromise;
+// export default clientPromise;
+
+
+
+
+
+// import { MongoClient, MongoClientOptions } from "mongodb";
+
+// const uri: string = process.env.MONGODB_URI!;
+// const options: MongoClientOptions = {};
+
+// declare global {
+//   // গ্লোবাল ভেরিয়েবল _mongoClientPromise টাইপড করা
+//   var _mongoClientPromise: Promise<MongoClient> | undefined;
+// }
+
+// let client: MongoClient;
+// let clientPromise: Promise<MongoClient>;
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error("Please add your Mongo URI to .env.local");
+// }
+
+// if (process.env.NODE_ENV === "development") {
+//   // ডেভেলপমেন্ট মোডে
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   // প্রোডাকশন মোডে
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
+
+// export default clientPromise;
