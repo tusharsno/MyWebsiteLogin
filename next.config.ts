1a8ce8297d066742c2d44087edfd9824fa6a5ed0
Tushar Barua <tusharbarua269@gmail.com>
2025-10-18 19:52:13 +0600

    Update sparkles animation and next.config setting

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	reactStrictMode: true,
-	compiler: {
-		removeConsole: process.env.NODE_ENV === 'production',
-	},
-	images: {
-		formats: ['image/webp', 'image/avif'],
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
