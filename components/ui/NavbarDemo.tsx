34b5e1a5cbf06fd2611df2aa5d907ae23c31c208
Tushar Barua <tusharbarua269@gmail.com>
2025-10-25 15:15:22 +0600

    build error fix

--- components/ui/NavbarDemo.tsx
+++ components/ui/NavbarDemo.tsx
@@ -77,6 +77,7 @@ export function NavbarDemo() {
               </a>
             ))}
             <div className="flex w-full flex-col gap-4">
+              <ThemeToggle />
               <NavbarButton
                 onClick={() => setIsMobileMenuOpen(false)}
                 variant="primary"
